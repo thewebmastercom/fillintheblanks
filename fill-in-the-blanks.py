2d8acf1f72fdb47ec3a43e7c3bc258a5fd6b7055
Jonathan Griffin <jonathan@thewebmaster.com>
2017-02-25 15:31:00 +0000

    second attempt

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -22,30 +22,43 @@ hard_answers = ['hardanswer1', 'answer2', 'answer3', 'answer4']
 print "Welcome to the fill in the blanks quiz"
 print "Levels available: Easy, Medium, Hard"
 
-def validate_user_level_choice():
+# Change this to take two arguments so can be reused to validate answers - TODO add guess limits
+
+# two functions = 1. validation correct input, and limits 2. question & answer correct
+
+def validate_input(userinput, validation):
+    if userinput.lower() in validation:
+        for e in validation:
+            if userinput == e:
+                return True
+            else:
+                return False
+    else:
+        return False
+
+def choose_level():
+    global quiz, answers
     user_level_choice_valid = ['easy', 'medium', 'hard']
     user_level_choice = raw_input("Please choose your difficulty level: ")
-    if user_level_choice.lower() in user_level_choice_valid:
-        choose_level(user_level_choice)
-    else:
-        print "Sorry this answer is not correct"
-        validate_user_level_choice()
+    print user_level_choice
 
-def choose_level(level):
-    global quiz, answers
-    if level == "easy":
-        quiz = easy_quiz
-        answers = easy_answers
-    else:
-        if level == "medium":
-            quiz = medium_quiz
-            answers = medium_answers
+    if validate_input(user_level_choice, user_level_choice_valid) == True:
+        if user_level_choice == "easy":
+            quiz = easy_quiz
+            answers = easy_answers
         else:
-            if level == "hard":
-                quiz = hard_quiz
-                answers = hard_answers
+            if user_level_choice == "medium":
+                quiz = medium_quiz
+                answers = medium_answers
+            else:
+                if user_level_choice == "hard":
+                    quiz = hard_quiz
+                    answers = hard_answers
+    else:
+        print "I am sorry but your choice was not a valid input"
+        choose_level()
+
     return quiz, answers
 
-validate_user_level_choice()
+choose_level()
 print quiz
-print answers
