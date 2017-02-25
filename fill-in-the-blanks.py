c3d11f3e95304bee18abe874daf1f198e2c0f7b3
Jonathan Griffin <jonathan@thewebmaster.com>
2017-02-25 13:05:09 +0000

    Completed the user level choice function

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -1,36 +1,51 @@
-# IPND Stage 2 Final Project
-
-# You've built a Mad-Libs game with some help from Sean.
-# Now you'll work on your own game to practice your skills and demonstrate what you've learned.
-
-# For this project, you'll be building a Fill-in-the-Blanks quiz.
-# Your quiz will prompt a user with a paragraph containing several blanks.
-# The user should then be asked to fill in each blank appropriately to complete the paragraph.
-# This can be used as a study tool to help you remember important vocabulary!
-
-# Note: Your game will have to accept user input so, like the Mad Libs generator,
-# you won't be able to run it using Sublime's `Build` feature.
-# Instead you'll need to run the program in Terminal or IDLE.
-# Refer to Work Session 5 if you need a refresher on how to do this.
-
-# To help you get started, we've provided a sample paragraph that you can use when testing your code.
-# Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
-
-sample = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
+easy_quiz = '''EASY - A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
 adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
 don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
 tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
 
-# The answer for ___1___ is 'function'. Can you figure out the others?
+easy_answers = ['easyanswer1', 'answer2', 'answer3', 'answer4']
 
-# We've also given you a file called fill-in-the-blanks.pyc which is a working version of the project.
-# A .pyc file is a Python file that has been translated into "byte code".
-# This means the code will run the same as the original .py file, but when you open it
-# it won't look like Python code! But you can run it just like a regular Python file
-# to see how your code should behave.
+medium_quiz = '''MEDIUM - A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
+adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
+don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
+tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
 
-# Hint: It might help to think about how this project relates to the Mad Libs generator you built with Sean.
-# In the Mad Libs generator, you take a paragraph and replace all instances of NOUN and VERB.
-# How can you adapt that design to work with numbered blanks?
+medium_answers = ['mediumanswer1', 'answer2', 'answer3', 'answer4']
 
-# If you need help, you can sign up for a 1 on 1 coaching appointment: https://calendly.com/ipnd-1-1/20min/
+hard_quiz = '''HARD - A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
+adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
+don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
+tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
+
+hard_answers = ['hardanswer1', 'answer2', 'answer3', 'answer4']
+
+print "Welcome to the fill in the blanks quiz"
+print "Levels available: Easy, Medium, Hard"
+
+def validate_user_level_choice():
+    user_level_choice_valid = ['easy', 'medium', 'hard']
+    user_level_choice = raw_input("Please choose your difficulty level: ")
+    if user_level_choice.lower() in user_level_choice_valid:
+        choose_level(user_level_choice)
+    else:
+        print "Sorry this answer is not correct"
+        validate_user_level_choice()
+
+def choose_level(level):
+    global quiz, answers
+    if level == "easy":
+        quiz = easy_quiz
+        answers = easy_answers
+    else:
+        if level == "medium":
+            quiz = medium_quiz
+            answers = medium_answers
+        else:
+            if level == "hard":
+                quiz = hard_quiz
+                answers = hard_answers
+    return quiz, answers
+
+validate_user_level_choice()
+print quiz
+print answers
